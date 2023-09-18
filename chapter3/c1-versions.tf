de1f5da36fcf5f0a765e90c3ec4e2495f34eabf5
Aeriel <aerielellisk5@gmail.com>
2023-09-18 06:19:48 -0700

    launch ec2 instance

--- chapter3/c1-versions.tf
+++ chapter3/c1-versions.tf
@@ -3,8 +3,8 @@ terraform {
   required_version = "~> 1.5.2" # allows 1.5.1, deny 1.6.2
   required_providers {
     aws = {
-        source = "value"
-        version = "value"
+      source = "hashicorp/aws"
+      version = ">= 4.65"
     }
   }
 }
@@ -14,3 +14,13 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# resource "aws_vpc" "vpc_us-west-1" {
+#     #  resource resource_type resource_local_name
+#   provider = aws.aws-west-1
+#   # provider, for_each, count --> are called meta-arguements; will change the behavior of the resource
+#   cidr_block = "10.2.0.0/16"
+#   tags = {
+#     "name" = "vpc-1"
+#   }
+# }
